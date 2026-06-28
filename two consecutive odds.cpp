b50c19103eaff803fb5bbbd5fa4e11493f697566
Piyush Deshmukh <piyush.deshmukh674@gmail.com>
2026-06-28 19:23:11 +0530

    Implement function to check for two consecutive odd numbers

---
+++ two consecutive odds.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+bool twoconsecutiveodds(vector<int> &arr){
+  int n=arr.size();
+  int count=0;
+  for(int i=0;i<n;i++){
+    if(arr[i]%2!=0){
+      count++;
+      if(count==2) return true;
+    } else {
+      count=0;
+    }
+  }
+  return false;
+  }
+};
+int main(){
+  vector<int> arr={1,2,1};
+  Solution sol;
+  cout << sol.twoconsecutiveodds(arr) << endl;
+  return 0;
+}
